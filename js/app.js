f8036c52f32f9ceb570de33cbbf8e0245fa87cba
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-04 19:01:58 -0700

    commit made header and table

--- js/app.js
+++ js/app.js
@@ -1,13 +1,138 @@
 class Cards {
-    constructor(number, suit, color) {
+    constructor(number, suit) {
         this.number = number;
         this.suit = suit;
-        this.color = color
     }
 }
 
-const twoOfHearts = new Cards(2, 'hearts', 'red');
-const threeOfHearts = new Cards(3, 'hearts', 'red');
-const fourOfHearts = new Cards(4, 'hearts', 'red');
-const fiveOfHearts = new Cards(5, 'hearts', 'red');
-const sixOfHearts = new Cards(6, 'hearts', 'red');
+class Hearts extends Cards {
+    constructor(number, suit) {
+        super(number, suit);
+        this.suit = 'hearts';
+    }
+} 
+
+const twoOfHearts = new Hearts(2);
+const threeOfHearts = new Hearts(3); 
+const fourOfHearts = new Hearts(4);
+const fiveOfHearts = new Hearts(5);
+const sixOfHearts = new Hearts(6);
+const sevenOfHearts = new Hearts(7);
+const eigthOfHearts = new Hearts(8);
+const nineOfHearts = new Hearts(9);
+const tenOfHearts = new Hearts(10);
+const jackOfHearts = new Hearts(10);
+const queenOfHearts = new Hearts(10);
+const kingOfHearts = new Hearts(10);
+const aceOfHearts = new Hearts(1 && 11);
+
+const hearts = [twoOfHearts, threeOfHearts, fourOfHearts, 
+fiveOfHearts, sixOfHearts, sevenOfHearts, eigthOfHearts, nineOfHearts, 
+tenOfHearts, jackOfHearts, queenOfHearts, kingOfHearts, aceOfHearts];
+
+console.log(hearts);
+
+class Diamonds extends Cards {
+    constructor(number, suit) {
+        super(number, suit);
+        this.suit = 'diamonds';
+    }
+}
+
+const twoOfDiamonds = new Diamonds(2);
+const threeOfDiamonds = new Diamonds(3);
+const fourOfDiamonds = new Diamonds(4);
+const fiveOfDiamonds = new Diamonds(5);
+const sixOfDiamonds = new Diamonds(6);
+const sevenOfDiamonds = new Diamonds(7);
+const eightOfDiamonds = new Diamonds(8);
+const nineOfDiamonds = new Diamonds(9);
+const tenOfDiamonds = new Diamonds(10);
+const jackOfDiamonds = new Diamonds(10);
+const queenOfDiamonds = new Diamonds(10);
+const kingOfDiamonds = new Diamonds(10);
+const aceOfDiamonds = new Diamonds(1 && 11);
+
+const diamonds = [twoOfDiamonds, threeOfDiamonds, fourOfDiamonds,
+fiveOfDiamonds, sixOfDiamonds, sevenOfDiamonds, eightOfDiamonds, 
+nineOfDiamonds, tenOfDiamonds, jackOfDiamonds, queenOfDiamonds,
+kingOfDiamonds, aceOfDiamonds];
+    
+console.log(diamonds);
+
+class Clubs extends Cards {
+    constructor(number, suit) {
+        super(number, suit);
+        this.suit = 'clubs';
+    }
+}
+
+const twoOfClubs = new Clubs(2);
+const threeOfClubs = new Clubs(3);
+const fourOfClubs = new Clubs(4);
+const fiveOfClubs = new Clubs(5);
+const sixOfClubs = new Clubs(6);
+const sevenOfClubs = new Clubs(7);
+const eigthOfClubs = new Clubs(8);
+const nineOfClubs = new Clubs(9);
+const tenOfClubs = new Clubs(10);
+const jackOfClubs = new Clubs(10);
+const queenOfClubs = new Clubs(10);
+const kingOfClubs = new Clubs(10);
+const aceOfClubs = new Clubs(1  && 11);
+
+const clubs = [twoOfClubs, threeOfClubs, fourOfClubs, fiveOfClubs, 
+sixOfClubs, sevenOfClubs, eigthOfClubs, nineOfDiamonds, tenOfClubs, 
+jackOfClubs, queenOfClubs, kingOfClubs, aceOfClubs];
+
+console.log(clubs);
+
+class Spades extends Cards {
+    constructor(number, suit) {
+        super(number, suit);
+        this.suit = 'spades';
+    }
+}
+
+const twoOfSpades = new Spades(2);
+const threeOfSpades = new Spades(3);
+const fourOfSpades = new Spades(4);
+const fiveOfSpades = new Spades(5);
+const sixOfSpades = new Spades(6);
+const sevenOfSpades = new Spades(7);
+const eightOfSpades = new Spades(8);
+const nineOfSpades = new Spades(9);
+const tenOfSpades = new Spades(10);
+const jackOfSpades = new Spades(10);
+const queenOfSpades = new Spades(10);
+const kingOfSpades = new Spades(10);
+const aceOfSpades = new Spades(1 && 11);
+
+const spades = [twoOfSpades, threeOfSpades, fourOfSpades, fiveOfSpades,
+sixOfSpades, sevenOfSpades, eightOfSpades, nineOfSpades, tenOfSpades,
+jackOfSpades, queenOfSpades, kingOfSpades, aceOfSpades];
+
+const reshufflePile = []
+
+const visibleCard = Math.floor(Math.random() * hearts.length);
+reshufflePile.push(visibleCard);
+console.log(visibleCard);
+console.log(reshufflePile);
+
+function matchHearts() {
+    if (2 ===2 && hearts) {
+    }
+    if (3 = 3 && hearts) {
+
+    }
+    if (4 ===4 && hearts) {
+    }
+    if (2 ===2 && hearts) {
+    
+
+}
+function pickCard() {
+}
+
+
+
