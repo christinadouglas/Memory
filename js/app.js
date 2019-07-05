81f4b249759756fbb8a85dbe9dd574c217ce3987
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-04 17:12:49 -0700

    commit made class cards

---
+++ js/app.js
@@ -0,0 +1,13 @@
+class Cards {
+    constructor(number, suit, color) {
+        this.number = number;
+        this.suit = suit;
+        this.color = color
+    }
+}
+
+const twoOfHearts = new Cards(2, 'hearts', 'red');
+const threeOfHearts = new Cards(3, 'hearts', 'red');
+const fourOfHearts = new Cards(4, 'hearts', 'red');
+const fiveOfHearts = new Cards(5, 'hearts', 'red');
+const sixOfHearts = new Cards(6, 'hearts', 'red');
