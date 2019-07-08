72ae20c57656412bba29a4c74227cd9633e88d6b
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-08 16:51:05 -0700

    Commit getting random card

--- js/app.js
+++ js/app.js
@@ -1,110 +1,110 @@
-class Cards {
-    constructor(number, suit) {
-        this.number = number;
-        this.suit = suit;
-    }
-}
+// class Cards {
+//     constructor(number, suit) {
+//         this.number = number;
+//         this.suit = suit;
+//     }
+// }
 
-class Hearts extends Cards {
-    constructor(number, suit) {
-        super(number, suit);
-        this.suit = 'hearts';
-    }
-} 
+// class Hearts extends Cards {
+//     constructor(number, suit) {
+//         super(number, suit);
+//         this.suit = 'hearts';
+//     }
+// } 
 
-const twoOfHearts = new Hearts(2);
-const threeOfHearts = new Hearts(3); 
-const fourOfHearts = new Hearts(4);
-const fiveOfHearts = new Hearts(5);
-const sixOfHearts = new Hearts(6);
-const sevenOfHearts = new Hearts(7);
-const eightOfHearts = new Hearts(8);
-const nineOfHearts = new Hearts(9);
-const tenOfHearts = new Hearts(10);
-const jackOfHearts = new Hearts(10);
-const queenOfHearts = new Hearts(10);
-const kingOfHearts = new Hearts(10);
-const aceOfHearts = new Hearts(1 && 11);
+// const twoOfHearts = new Hearts(2);
+// const threeOfHearts = new Hearts(3); 
+// const fourOfHearts = new Hearts(4);
+// const fiveOfHearts = new Hearts(5);
+// const sixOfHearts = new Hearts(6);
+// const sevenOfHearts = new Hearts(7);
+// const eightOfHearts = new Hearts(8);
+// const nineOfHearts = new Hearts(9);
+// const tenOfHearts = new Hearts(10);
+// const jackOfHearts = new Hearts(10);
+// const queenOfHearts = new Hearts(10);
+// const kingOfHearts = new Hearts(10);
+// const aceOfHearts = new Hearts(1 && 11);
 
-const hearts = [twoOfHearts, threeOfHearts, fourOfHearts, 
-fiveOfHearts, sixOfHearts, sevenOfHearts, eightOfHearts, nineOfHearts, 
-tenOfHearts, jackOfHearts, queenOfHearts, kingOfHearts, aceOfHearts];
+// const hearts = [twoOfHearts, threeOfHearts, fourOfHearts, 
+// fiveOfHearts, sixOfHearts, sevenOfHearts, eightOfHearts, nineOfHearts, 
+// tenOfHearts, jackOfHearts, queenOfHearts, kingOfHearts, aceOfHearts];
 
-class Diamonds extends Cards {
-    constructor(number, suit) {
-        super(number, suit);
-        this.suit = 'diamonds';
-    }
-}
+// class Diamonds extends Cards {
+//     constructor(number, suit) {
+//         super(number, suit);
+//         this.suit = 'diamonds';
+//     }
+// }
 
-const twoOfDiamonds = new Diamonds(2);
-const threeOfDiamonds = new Diamonds(3);
-const fourOfDiamonds = new Diamonds(4);
-const fiveOfDiamonds = new Diamonds(5);
-const sixOfDiamonds = new Diamonds(6);
-const sevenOfDiamonds = new Diamonds(7);
-const eightOfDiamonds = new Diamonds(8);
-const nineOfDiamonds = new Diamonds(9);
-const tenOfDiamonds = new Diamonds(10);
-const jackOfDiamonds = new Diamonds(10);
-const queenOfDiamonds = new Diamonds(10);
-const kingOfDiamonds = new Diamonds(10, 'king');
-const aceOfDiamonds = new Diamonds(1 && 11);
+// const twoOfDiamonds = new Diamonds(2);
+// const threeOfDiamonds = new Diamonds(3);
+// const fourOfDiamonds = new Diamonds(4);
+// const fiveOfDiamonds = new Diamonds(5);
+// const sixOfDiamonds = new Diamonds(6);
+// const sevenOfDiamonds = new Diamonds(7);
+// const eightOfDiamonds = new Diamonds(8);
+// const nineOfDiamonds = new Diamonds(9);
+// const tenOfDiamonds = new Diamonds(10);
+// const jackOfDiamonds = new Diamonds(10);
+// const queenOfDiamonds = new Diamonds(10);
+// const kingOfDiamonds = new Diamonds(10, 'king');
+// const aceOfDiamonds = new Diamonds(1 && 11);
 
-const diamonds = [twoOfDiamonds, threeOfDiamonds, fourOfDiamonds,
-fiveOfDiamonds, sixOfDiamonds, sevenOfDiamonds, eightOfDiamonds, 
-nineOfDiamonds, tenOfDiamonds, jackOfDiamonds, queenOfDiamonds,
-kingOfDiamonds, aceOfDiamonds];
+// const diamonds = [twoOfDiamonds, threeOfDiamonds, fourOfDiamonds,
+// fiveOfDiamonds, sixOfDiamonds, sevenOfDiamonds, eightOfDiamonds, 
+// nineOfDiamonds, tenOfDiamonds, jackOfDiamonds, queenOfDiamonds,
+// kingOfDiamonds, aceOfDiamonds];
 
-class Clubs extends Cards {
-    constructor(number, suit) {
-        super(number, suit);
-        this.suit = 'clubs';
-    }
-}
+// class Clubs extends Cards {
+//     constructor(number, suit) {
+//         super(number, suit);
+//         this.suit = 'clubs';
+//     }
+// }
 
-const twoOfClubs = new Clubs(2);
-const threeOfClubs = new Clubs(3);
-const fourOfClubs = new Clubs(4);
-const fiveOfClubs = new Clubs(5);
-const sixOfClubs = new Clubs(6);
-const sevenOfClubs = new Clubs(7);
-const eigthOfClubs = new Clubs(8);
-const nineOfClubs = new Clubs(9);
-const tenOfClubs = new Clubs(10);
-const jackOfClubs = new Clubs(10);
-const queenOfClubs = new Clubs(10);
-const kingOfClubs = new Clubs(10, 'king');
-const aceOfClubs = new Clubs(1  && 11);
+// const twoOfClubs = new Clubs(2);
+// const threeOfClubs = new Clubs(3);
+// const fourOfClubs = new Clubs(4);
+// const fiveOfClubs = new Clubs(5);
+// const sixOfClubs = new Clubs(6);
+// const sevenOfClubs = new Clubs(7);
+// const eigthOfClubs = new Clubs(8);
+// const nineOfClubs = new Clubs(9);
+// const tenOfClubs = new Clubs(10);
+// const jackOfClubs = new Clubs(10);
+// const queenOfClubs = new Clubs(10);
+// const kingOfClubs = new Clubs(10, 'king');
+// const aceOfClubs = new Clubs(1  && 11);
 
-const clubs = [twoOfClubs, threeOfClubs, fourOfClubs, fiveOfClubs, 
-sixOfClubs, sevenOfClubs, eigthOfClubs, nineOfClubs, tenOfClubs, 
-jackOfClubs, queenOfClubs, kingOfClubs, aceOfClubs];
+// const clubs = [twoOfClubs, threeOfClubs, fourOfClubs, fiveOfClubs, 
+// sixOfClubs, sevenOfClubs, eigthOfClubs, nineOfClubs, tenOfClubs, 
+// jackOfClubs, queenOfClubs, kingOfClubs, aceOfClubs];
 
-class Spades extends Cards {
-    constructor(number, suit) {
-        super(number, suit);
-        this.suit = 'spades';
-    }
-}
+// class Spades extends Cards {
+//     constructor(number, suit) {
+//         super(number, suit);
+//         this.suit = 'spades';
+//     }
+// }
 
-const twoOfSpades = new Spades(2);
-const threeOfSpades = new Spades(3);
-const fourOfSpades = new Spades(4);
-const fiveOfSpades = new Spades(5);
-const sixOfSpades = new Spades(6);
-const sevenOfSpades = new Spades(7);
-const eightOfSpades = new Spades(8);
-const nineOfSpades = new Spades(9);
-const tenOfSpades = new Spades(10);
-const jackOfSpades = new Spades(10);
-const queenOfSpades = new Spades(10);
-const kingOfSpades = new Spades(10);
-const aceOfSpades = new Spades(1 && 11);
+// const twoOfSpades = new Spades(2);
+// const threeOfSpades = new Spades(3);
+// const fourOfSpades = new Spades(4);
+// const fiveOfSpades = new Spades(5);
+// const sixOfSpades = new Spades(6);
+// const sevenOfSpades = new Spades(7);
+// const eightOfSpades = new Spades(8);
+// const nineOfSpades = new Spades(9);
+// const tenOfSpades = new Spades(10);
+// const jackOfSpades = new Spades(10);
+// const queenOfSpades = new Spades(10);
+// const kingOfSpades = new Spades(10);
+// const aceOfSpades = new Spades(1 && 11);
 
-const spades = [twoOfSpades, threeOfSpades, fourOfSpades, fiveOfSpades,
-sixOfSpades, sevenOfSpades, eightOfSpades, nineOfSpades, tenOfSpades,
-jackOfSpades, queenOfSpades, kingOfSpades, aceOfSpades];
+// const spades = [twoOfSpades, threeOfSpades, fourOfSpades, fiveOfSpades,
+// sixOfSpades, sevenOfSpades, eightOfSpades, nineOfSpades, tenOfSpades,
+// jackOfSpades, queenOfSpades, kingOfSpades, aceOfSpades];
 
 const timerSpan = document.querySelector("#timer");
 
@@ -191,15 +191,15 @@ fourC, fourD, fourH, fourS, fiveC, fiveD, fiveH, fiveS, sixC, sixD, sixH,
 sixS, sevenC, sevenD, sevenH, sevenS, eightC, eightD, eightH, eightS, 
 nineC, nineD, nineH, nineS, tenC, tenD, tenH, tenS, jS, jC, jD, jH, qH,
 qS, qD, qC, kC, kH, kD, kC, aC, aD, aH, aS]
-// const game = () => {
 
-// function deck(){
-//     let num = Math.floor(Math.random() * cardImages.length); 
-//     cardOne = cardImages[num];
-//     };
-// deck();
-
-// game.deck();
+const game = () => {
+function deck(){
+    let cardOne = Math.floor(Math.random() * cards.length); 
+    console.log(cardOne)
+    };
+    deck();
+}
+game();
 
 // const twoUpper = document.querySelector("#twoUpper");
 
